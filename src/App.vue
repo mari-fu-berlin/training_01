4ba6b9502a0497c51601c008cd6652b2bf983143
culturetogo <m.mueller@culture-to-go.de>
2017-10-04 11:23:32 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <app-content></app-content>
+    </div>
+</template>
+
+<script>
+// Imports
+import content from './components/content.vue';
+
+export default {
+  name: 'app',
+  components: {
+      'app-content': content
+  },
+  data () {
+    return {
+      msg: 'Mein Titel'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/content.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="content">
+        <p>{{contentBody}}</p>
+    </div><!-- #content -->
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      contentBody: 'Content aus ContentBody',
+    }
+  },
+  created(){
+    axios.get("http://inhalte.mari-intern.de/wp-json/wp/v2/pages/53")
+      .then(function(response){
+        console.log(response);
+      })
+  }
+}
+
+
+</script>
+
+<style scoped>
+#content {
+  color: #aaa;
+}
+</style>
